4a041c3c54fb75560a3b8a9c3ab4c3bbb1d438ca
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2025-11-16 10:19:16 +0530

    Created getall student methord

--- src/main/java/org/example/controller/StudentController.java
+++ src/main/java/org/example/controller/StudentController.java
@@ -1,12 +1,23 @@
 package org.example.controller;
 
+import org.example.model.dto.Student;
+import org.example.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
 
+    @Autowired
+    StudentService service;
 
-
+    @GetMapping("/all")
+    public ArrayList<Student> getAll() {
+        return service.getAll();
+    }
 }
--- src/main/java/org/example/service/StudentService.java
+++ src/main/java/org/example/service/StudentService.java
@@ -1,7 +1,33 @@
 package org.example.service;
 
+import org.example.model.dto.Student;
+import org.example.model.entity.StudentEntity;
+import org.example.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class StudentService {
+
+    @Autowired
+    StudentRepository repository;
+
+    ArrayList<Student> all=new ArrayList<>();
+
+    public ArrayList<Student> getAll() {
+        for(StudentEntity studentEntity: repository.findAll()){
+            Student student= new Student(
+                    studentEntity.getId(),
+                    studentEntity.getName(),
+                    studentEntity.getContactNumber(),
+                    studentEntity.getEmail(),
+                    studentEntity.getDate(),
+                    studentEntity.getAddress()
+            );
+            all.add(student);
+        }
+        return all;
+    }
 }
